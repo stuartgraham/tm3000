310acb1c244b85d8d0f8eb1279aed2d1d2ccfc36
Stuart Graham <stuart@stuart-graham.com>
2024-03-01 23:19:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import paho.mqtt.client as mqtt
+
+MQTT_HOST = os.environ.get("MQTT_HOST", "")
+MQTT_PORT = os.environ.get("MQTT_PORT", 1883)
+MQTT_USER = os.environ.get("MQTT_USER", "")
+MQTT_PASSWORD = os.environ.get("MQTT_PASSWORD", "")
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    client.subscribe("bathroom-sensor/sensor/bme680_iaq/state")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+mqttc.connect(MQTT_HOST, MQTT_PORT, 60)
+
+mqttc.loop_forever()
