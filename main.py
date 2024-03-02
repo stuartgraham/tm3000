e1c10231046a18f7eb0250d1631edfbec2de47cd
Stuart Graham <stuart@stuart-graham.com>
2024-03-02 00:00:59 +0000

    feat: add sensor subs

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ MQTT_PASSWORD = os.environ.get("MQTT_PASSWORD", "")
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
     client.subscribe("bathroom-sensor/sensor/bme680_iaq/state")
+    client.subscribe("bathroom-sensor/sensor/bme680_gas_resistance/state")
+    client.subscribe("bathroom-sensor/sensor/bme680_temperature/state")
+    client.subscribe("bathroom-sensor/sensor/bme680_humidity/state")
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
