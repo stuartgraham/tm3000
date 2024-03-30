b9c48420ad0296c9cf80735e1c218e019af4ce8b
Stuart Graham <stuart@stuart-graham.com>
2024-03-30 15:33:42 +0000

    feat: add pendulum and topic calc

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import os
 import paho.mqtt.client as mqtt
+import pendulum
 
 MQTT_HOST = os.environ.get("MQTT_HOST", "")
 MQTT_PORT = int(os.environ.get("MQTT_PORT", 1883))
 MQTT_USER = os.environ.get("MQTT_USER", "")
 MQTT_PASSWORD = os.environ.get("MQTT_PASSWORD", "")
+IAQ_TOPIC = {'path': 'bathroom-sensor/sensor/bme680_iaq/state', 'name': 'iaq'}
+GAS_TOPIC = {'path': 'bathroom-sensor/sensor/bme680_gas_resistance/state', 'name': 'gas'}
+TEMP_TOPIC = {'path': 'bathroom-sensor/sensor/bme680_temperature/state"', 'name': 'humidity'}
+HUMIDITY_TOPIC = {'path': 'bathroom-sensor/sensor/bme680_humidity/state', 'name': 'temperature'}
+TOPIC_LIST = [IAQ_TOPIC, GAS_TOPIC, TEMP_TOPIC, HUMIDITY_TOPIC]
 
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    client.subscribe("bathroom-sensor/sensor/bme680_iaq/state")
-    client.subscribe("bathroom-sensor/sensor/bme680_gas_resistance/state")
-    client.subscribe("bathroom-sensor/sensor/bme680_temperature/state")
-    client.subscribe("bathroom-sensor/sensor/bme680_humidity/state")
-
+    for topic in TOPIC_LIST:
+        client.subscribe(topic['path'])
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(pendulum.now())
+    print(check_topic(msg.topic))
+    print(str(msg.payload))
+
+
+def check_topic(input_topic):
+    calculated_topic = 'null'
+    for topic in TOPIC_LIST:
+        if topic['path'] == input_topic:
+            calculated_topic = topic['name']
+    return calculated_topic
 
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
